bcf4c7e80387ccf5a4922c47492714cfb986e876
Andrei <taysan003@gmail.com>
2018-08-17 02:49:47 -0400

    hooks cucumber junit

---
+++ FreeCrmBDDFFramework/src/test/java/stepDefinitions/TaggedHooksDefinition.java
@@ -0,0 +1,78 @@
+package stepDefinitions;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class TaggedHooksDefinition {
+	
+	@Before()
+	public void setUP(){
+		System.out.println("launch FF  @Before()");
+		System.out.println("Enter URL for Free CRM APP  @Before()");
+	}
+
+	@After()
+	public void tearDown(){
+		System.out.println("close the browser @After()");
+	}
+	
+	@Before(order=1)
+	public void setUP1(){
+		System.out.println("launch FF  @Before(order=1)");
+		System.out.println("Enter URL for Free CRM APP  @Before(order=1)");
+	}
+
+	@After(order=1)
+	public void tearDown1(){
+		System.out.println("close the browser @After(order=1)");
+	}
+	
+	@Before("@First")
+	public void beforeFirst(){
+		System.out.println("before only first scenario @Before(\"@First\")");
+	}
+	
+	@After("@First")
+	public void afterFirst(){
+		System.out.println("after only first sceanrio  @After(\"@First\")");
+	}
+	
+	@Before("@Second")
+	public void beforeSecond(){
+		System.out.println("before only second scenario  @Before(\"@Second\")");
+	}
+	
+	@After("@Second")
+	public void afterSecond(){
+		System.out.println("after only second sceanrio  @After(\"@Second\")");
+	}
+	
+	@Before("@Third")
+	public void beforeThird(){
+		System.out.println("before only third scenario  @Before(\"@Third\")");
+	}
+	
+	@After("@Third")
+	public void afterThird(){
+		System.out.println("after only third sceanrio @After(\"@Third\")");
+	}
+	
+	
+	@Given("^this is the first step$")
+	public void this_is_the_first_step() throws Throwable {
+		System.out.println("1st step  @Given(\"^this is the first step$\")");
+	}
+
+	@When("^this is the second step$")
+	public void this_is_the_second_step() throws Throwable {
+		System.out.println("2nd step  @When(\"^this is the second step$\")");
+	}
+
+	@Then("^this is the third step$")
+	public void this_is_the_third_step() throws Throwable {
+		System.out.println("3rd step  @Then(\"^this is the third step$\")");
+	}
+}
--- FreeCrmBDDFFramework/test-outout/report.js
+++ FreeCrmBDDFFramework/test-outout/report.js
@@ -1,35 +1,231 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("G:/SeleniumProjects/FreeCrmBDDFramework/FreeCrmBDDFFramework/src/test/java/Features/tagging.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("G:/SeleniumProjects/FreeCrmBDDFramework/FreeCrmBDDFFramework/src/test/java/Features/taggedhooks.feature");
 formatter.feature({
-  "line": 2,
-  "name": "Free CRM application testing",
+  "line": 1,
+  "name": "Test Tagged Hooks",
   "description": "",
-  "id": "free-crm-application-testing",
-  "keyword": "Feature",
+  "id": "test-tagged-hooks",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 276543,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 109037,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 83358,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "This is First test",
+  "description": "",
+  "id": "test-tagged-hooks;this-is-first-test",
+  "type": "scenario",
+  "keyword": "Scenario",
   "tags": [
     {
-      "line": 1,
-      "name": "@FunctionalTest"
+      "line": 3,
+      "name": "@First"
     }
   ]
 });
-formatter.scenario({
-  "line": 74,
-  "name": "Browser closed",
-  "description": "",
-  "id": "free-crm-application-testing;browser-closed",
-  "type": "scenario",
-  "keyword": "Scenario"
-});
 formatter.step({
-  "line": 75,
-  "name": "This is a close browser test",
+  "line": 5,
+  "name": "this is the first step",
   "keyword": "Given "
 });
+formatter.step({
+  "line": 6,
+  "name": "this is the second step",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "this is the third step",
+  "keyword": "Then "
+});
 formatter.match({
-  "location": "TeggingStepDefinition.this_is_a_close_browser_test()"
+  "location": "TaggedHooksDefinition.this_is_the_first_step()"
 });
 formatter.result({
-  "duration": 77707883,
+  "duration": 83389662,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TaggedHooksDefinition.this_is_the_second_step()"
+});
+formatter.result({
+  "duration": 83358,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TaggedHooksDefinition.this_is_the_third_step()"
+});
+formatter.result({
+  "duration": 88889,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 85728,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 80593,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 90074,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 181728,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 94024,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 89680,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 10,
+  "name": "This is Second test",
+  "description": "",
+  "id": "test-tagged-hooks;this-is-second-test",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 9,
+      "name": "@Second"
+    }
+  ]
+});
+formatter.step({
+  "line": 11,
+  "name": "this is the first step",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 12,
+  "name": "this is the second step",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 13,
+  "name": "this is the third step",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "TaggedHooksDefinition.this_is_the_first_step()"
+});
+formatter.result({
+  "duration": 62815,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TaggedHooksDefinition.this_is_the_second_step()"
+});
+formatter.result({
+  "duration": 67950,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TaggedHooksDefinition.this_is_the_third_step()"
+});
+formatter.result({
+  "duration": 123654,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 71507,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 69531,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 44247,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 142617,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 60839,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 54124,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 16,
+  "name": "This is Third test",
+  "description": "",
+  "id": "test-tagged-hooks;this-is-third-test",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 15,
+      "name": "@Third"
+    }
+  ]
+});
+formatter.step({
+  "line": 17,
+  "name": "this is the first step",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 18,
+  "name": "this is the second step",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 19,
+  "name": "this is the third step",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "TaggedHooksDefinition.this_is_the_first_step()"
+});
+formatter.result({
+  "duration": 52938,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TaggedHooksDefinition.this_is_the_second_step()"
+});
+formatter.result({
+  "duration": 47407,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TaggedHooksDefinition.this_is_the_third_step()"
+});
+formatter.result({
+  "duration": 62025,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 49778,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 52543,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 46617,
   "status": "passed"
 });
 });
